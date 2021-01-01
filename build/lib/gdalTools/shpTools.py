fe7e95f62c6a5796ea1d231ecf06ec908644eb9e
SonwYang <1224425503@qq.com>
2021-01-01 15:13:38 +0800

    0.2

--- build/lib/gdalTools/shpTools.py
+++ build/lib/gdalTools/shpTools.py
@@ -148,6 +148,43 @@ def remove_big_feature(inputShp, outputShp, area_threshold):
     del dataSource, out_ds
 
 
+def remove_small_feature(inputShp, outputShp, area_threshold):
+    """
+    This function is used to remove small area of feature from shapefile
+    :param inputShp: the path of input shapefile
+    :param outputShp: the path of output shapefile
+    :param area_thresold: the threshold of area
+    :return:
+    """
+    driver = ogr.GetDriverByName("ESRI Shapefile")
+    dataSource = driver.Open(inputShp, 1)
+    layer = dataSource.GetLayer()
+    new_field = ogr.FieldDefn("Area", ogr.OFTReal)
+    new_field.SetWidth(32)
+    new_field.SetPrecision(16)  # 设置面积精度,小数点后16位
+    layer.CreateField(new_field)
+
+    # 新建DataSource，Layer
+    out_ds = driver.CreateDataSource(outputShp)
+    out_lyr = out_ds.CreateLayer(outputShp, layer.GetSpatialRef(), ogr.wkbPolygon)
+    def_feature = out_lyr.GetLayerDefn()
+
+    for feature in layer:
+        geom = feature.GetGeometryRef()
+        area = geom.GetArea()  # 计算面积
+        if area < area_threshold:
+            continue
+        feature.SetField("Area", area)  # 将面积添加到属性表中
+        layer.SetFeature(feature)
+        out_feature = ogr.Feature(def_feature)
+        out_feature.SetGeometry(geom)
+        out_lyr.CreateFeature(out_feature)
+        out_feature = None
+
+    out_ds.FlushCache()
+    del dataSource, out_ds
+
+
 def buffer(inShp, outShp, bdistance=0.02):
     """
         setting up buffer zone in shapefile
@@ -180,3 +217,31 @@ def buffer(inShp, outShp, bdistance=0.02):
     del in_ds, out_ds
 
 
+def smoothing(inShp, fname, bdistance=0.001):
+    """
+    :param inShp: the path of input shapefile
+    :param fname: the path of output shapefile
+    :param bdistance: the distance of buffer
+    :return:
+    """
+    ogr.UseExceptions()
+    in_ds = ogr.Open(inShp)
+    in_lyr = in_ds.GetLayer()
+    # 创建输出Buffer文件
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    if Path(fname).exists():
+        driver.DeleteDataSource(fname)
+    # 新建DataSource，Layer
+    out_ds = driver.CreateDataSource(fname)
+    out_lyr = out_ds.CreateLayer(fname, in_lyr.GetSpatialRef(), ogr.wkbPolygon)
+    def_feature = out_lyr.GetLayerDefn()
+    # 遍历原始的Shapefile文件给每个Geometry做Buffer操作
+    for feature in in_lyr:
+        geometry = feature.GetGeometryRef()
+        buffer = geometry.Buffer(bdistance).Buffer(-bdistance)
+        out_feature = ogr.Feature(def_feature)
+        out_feature.SetGeometry(buffer)
+        out_lyr.CreateFeature(out_feature)
+        out_feature = None
+    out_ds.FlushCache()
+    del in_ds, out_ds
